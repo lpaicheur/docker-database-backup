02c84ef375f6283e4c1e0801bf5e9a5f62ed14ad
lpaicheur <lpaicheur@users.noreply.github.com>
2018-02-25 22:54:27 +0700

    Initial script

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -14,6 +14,13 @@ EOF
   then
     echo "CLEANUP_OLDER_THAN=$CLEANUP_OLDER_THAN" >> backup-cron
   fi
+  if [ -n "$AWS_ACCESS_KEY_ID" ] && [ -n "$AWS_SECRET_ACCESS_KEY" ] && [ -n "$AWS_DEFAULT_REGION" ] && [ -n "$AWS_BUCKET_NAME" ];
+  then
+    echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+AWS_BUCKET_NAME=$AWS_BUCKET_NAME" >> backup-cron
+  fi
   echo "$BACKUP_TIME backup > /backup.log" >> backup-cron
 
   crontab backup-cron
